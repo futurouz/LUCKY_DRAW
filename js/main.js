518c056896eb4539f15b74f18307dd61d6bf8439
Kunanan <kunanan.tw@gmail.com>
2018-07-06 00:36:17 +0700

    add remove if got select

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
   let data;
   let shuffleData;
   let isRandoming = false;
-
+  let idx_1, idx_2, idx_3, idx_4, idx_5;
+  let names;
   fetch(
     'https://script.google.com/macros/s/AKfycbw8a_uMecRi7RBLtKTqdM-FCjUKzIZqEPcWQlH80Oo-xgJ8b_mt/exec'
   )
@@ -16,57 +17,58 @@
       shuffleData = data.sort(() => {
         return 0.5 - Math.random();
       });
+      names = shuffleData;
     });
 
   function startRolling() {
-    var names = shuffleData;
     var start_idx = $('#name_list').data('start_idx');
-    var idx_1 = start_idx % names.length;
-    var idx_2 = (start_idx + 1) % names.length;
-    var idx_3 = (start_idx + 2) % names.length;
-    var idx_4 = (start_idx + 3) % names.length;
-    var idx_5 = (start_idx + 4) % names.length;
-    $('#draw-1 .text').text(names[idx_1]);
-    $('#draw-2 .text').text(names[idx_2]);
-    $('#draw-3 .text').text(names[idx_3]);
-    $('#draw-4 .text').text(names[idx_4]);
-    $('#draw-5 .text').text(names[idx_5]);
-
-    $('#name_list').data('start_idx', idx_2);
+    if (typeof names !== 'undefined') {
+      idx_1 = start_idx % names.length;
+      idx_2 = (start_idx + 1) % names.length;
+      idx_3 = (start_idx + 2) % names.length;
+      idx_4 = (start_idx + 3) % names.length;
+      idx_5 = (start_idx + 4) % names.length;
+      $('#draw-1 .text').text(names[idx_1]);
+      $('#draw-2 .text').text(names[idx_2]);
+      $('#draw-3 .text').text(names[idx_3]);
+      $('#draw-4 .text').text(names[idx_4]);
+      $('#draw-5 .text').text(names[idx_5]);
+      $('#name_list').data('start_idx', idx_2);
+    }
   }
   $('body').keyup(e => {
     if (e.keyCode == 32) {
       isRandoming = !isRandoming;
       if (isRandoming) {
         $('.draw-c').addClass('active');
-        names = parseList('#name_list');
-        $('#name_list').data('names', names);
         $('#name_list').data('start_idx', 0);
-
-        intervalID = setInterval(startRolling, 50);
+        intervalID = setInterval(startRolling, 100);
       } else {
         clearInterval(intervalID);
+        if (typeof names !== 'undefined') {
+          names.splice(idx_3, 1);
+        }
         $('.draw-c').removeClass('active');
       }
     }
   });
 });
 
-function TrimString(sInString) {
-  sInString = sInString.replace(/^\s+/g, ''); // strip leading
-  return sInString.replace(/\s+$/g, ''); // strip trailing
-}
+// function TrimString(sInString) {
+//   sInString = sInString.replace(/^\s+/g, ''); // strip leading
+//   return sInString.replace(/\s+$/g, ''); // strip trailing
+// }
 
-function parseList(textarea) {
-  var list_str = $(textarea).val();
+// function parseList(textarea) {
+//   var list_str = $(textarea).val();
 
-  var names = new Array();
+//   var names = new Array();
 
-  var list_lines = list_str.split('\n');
-  for (var i = 0; i < list_lines.length; i++) {
-    var name = TrimString(list_lines[i]);
-    if (name != '') names.push(name);
-  }
+//   var list_lines = list_str.split('\n');
+//   for (var i = 0; i < list_lines.length; i++) {
+//     var name = TrimString(list_lines[i]);
+//     if (name != '') names.push(name);
+//   }
 
-  return names;
-}
+//   return names;
+// }
